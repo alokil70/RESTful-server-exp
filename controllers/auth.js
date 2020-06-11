ca1a4d48975d3c4677a7ebc76b1b7d7d9b5f8519
Alexandr <Alokil70@gmail.com>
2020-06-11 19:00:34 +0300

    split token

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,7 +75,7 @@ module.exports.logout = function(req, res) {
 module.exports.findUser = async function(req, res) {
 
     // const token = (req.headers.authorization).toString().substr(14)
-    const token = (req.headers.authorization).split(' ').slice(2).join()
+    const token = (req.headers.authorization).split(' ')[2]
     const decodeToken = jwt.decode(token)
 
     const findUser = await db.User.findOne({
